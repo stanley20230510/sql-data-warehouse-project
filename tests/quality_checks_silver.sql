2d968c990b7ed8f2b253000b3941708cf8cd31da
stanley20230510 <133093059+stanley20230510@users.noreply.github.com>
2026-03-21 15:39:28 +0800

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,26 @@
 
+--Check For Nulls or Duplicates in Primary Key
+--Expectation: No Result
+
+SELECT
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+--Check for unwanted Spaces
+--Expectation: No Results
+SELECT 
+    cst_lastname
+FROM [DataWarehouse].[silver].[crm_cust_info]
+WHERE cst_lastname != TRIM(cst_lastname)
+
+--Data Standardization & Consistency
+SELECT DISTINCT 
+    [cst_gndr]
+FROM [DataWarehouse].[silver].[crm_cust_info]
+
+SELECT *
+FROM silver.crm_prd_info
+
